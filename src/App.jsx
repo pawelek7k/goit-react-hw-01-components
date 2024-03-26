f2fa866f4943187ce20ae6629ffd6e141136dfd6
pawelek <k.pawelek@vp.pl>
2024-03-26 19:29:49 +0100

    add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import user from "../user.json";
 import "./App.css";
 import { UserProfile } from "./Profile/UserProfile";
 
 function App() {
   return (
     <>
-      <UserProfile></UserProfile>
+      <UserProfile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </>
   );
 }
--- src/Profile/UserProfile.jsx
+++ src/Profile/UserProfile.jsx
@@ -1,38 +1,43 @@
-type UserProfileProps = {
-  username: string,
-};
+import PropTypes from "prop-types";
 
-export const UserProfile = (props: UserProfileProps) => {
-  const { username } = props;
+export const UserProfile = ({ username, tag, location, avatar, stats }) => {
   return (
     <>
       <div className="profile">
         <div className="description">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-            alt="User avatar"
-            className="avatar"
-          />
-          <p className="name"></p>
-          <p className="tag">@pmarica</p>
-          <p className="location">Salvador, Brasil</p>
+          <img src={avatar} alt="User avatar" className="avatar" />
+          <p className="name">{username}</p>
+          <p className="tag">@{tag}</p>
+          <p className="location">{location}l</p>
         </div>
 
         <ul className="stats">
           <li>
             <span className="label">Followers</span>
-            <span className="quantity">1000</span>
+            <span className="quantity">{stats.followers}</span>
           </li>
           <li>
             <span className="label">Views</span>
-            <span className="quantity">2000</span>
+            <span className="quantity">{stats.views}</span>
           </li>
           <li>
             <span className="label">Likes</span>
-            <span className="quantity">3000</span>
+            <span className="quantity">{stats.likes}</span>
           </li>
         </ul>
       </div>
     </>
   );
+};
+
+UserProfile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
 };
