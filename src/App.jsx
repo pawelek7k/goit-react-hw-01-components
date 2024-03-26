d433e50000dee195a44b84fa757dd4dc2cdc5b1f
pawelek <k.pawelek@vp.pl>
2024-03-26 17:18:40 +0100

    add files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { userProfile } from "./components/userProfile";
+
+function App() {
+  return (
+    <>
+      <userProfile></userProfile>
+    </>
+  );
+}
+
+export default App;
