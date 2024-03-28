a671fc33a0ac2dc022e2265cb22569718d583a17
pawelek <k.pawelek@vp.pl>
2024-03-28 12:10:40 +0100

    add profile styles

--- src/components/Profile/UserProfileStyles.js
+++ src/components/Profile/UserProfileStyles.js
@@ -6,12 +6,12 @@ const ProfileContainer = styled.div`
   background-color: #f9f9f9;
   padding: 20px;
   flex-direction: column;
-  border-radius: 8px;
+  border-radius: 2rem;
+  width: 20rem;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  justify-content: center;
 
   .description {
-    margin-right: 20px;
-
     .avatar {
       border-radius: 50%;
       margin-bottom: 10px;
@@ -21,6 +21,9 @@ const ProfileContainer = styled.div`
       font-size: 20px;
       font-weight: bold;
       margin-bottom: 5px;
+      color: #000000ba;
+      letter-spacing: 2px;
+      text-align: center;
     }
 
     .tag {
@@ -35,17 +38,23 @@ const ProfileContainer = styled.div`
 
   .stats {
     list-style-type: none;
+    display: flex;
+    gap: 3rem;
+    align-items: center;
+    justify-content: center;
+    padding: 0px;
+    letter-spacing: 1px;
 
     li {
       display: flex;
       align-items: center;
+      flex-direction: column;
 
       margin-top: 10px;
 
       .label {
         font-weight: bold;
-        color: black;
-        margin-right: 10px;
+        color: #000000ba;
       }
 
       .quantity {
