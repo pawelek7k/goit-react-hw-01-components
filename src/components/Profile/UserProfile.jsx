d8d627fd221be1d326b0799c308678171826d5de
pawelek <k.pawelek@vp.pl>
2024-03-28 18:04:36 +0100

    add .isRequired

--- src/components/Profile/UserProfile.jsx
+++ src/components/Profile/UserProfile.jsx
@@ -33,13 +33,13 @@ export const UserProfile = ({ username, tag, location, avatar, stats }) => {
 };
 
 UserProfile.propTypes = {
-  username: PropTypes.string,
-  tag: PropTypes.string,
-  location: PropTypes.string,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
   avatar: PropTypes.string,
   stats: PropTypes.shape({
-    followers: PropTypes.number,
-    views: PropTypes.number,
-    likes: PropTypes.number,
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
   }),
 };
